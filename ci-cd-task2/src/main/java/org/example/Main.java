e2295abe22fed4031b01f5296c6dd13a65fb3a8a
Nurzat Zhamalbek kyzy <68752315+nurzat0803@users.noreply.github.com>
2023-11-13 12:33:41 +0600

    ci-ci project's files were uploaded
    yes

---
+++ ci-cd-task2/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("- This is the task that considers ci/cd project requirements. And apply all requirements!!!\n" +
+                "- I think that is the optimum way to overcome all my feelings. Just let the work to done in my life\n" +
+                "- I will continue to growth my career!!!\n" +
+                "- I will earn more money for just myself\n");
+    }
+}
